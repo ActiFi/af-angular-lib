048a838facbec1eee838a6e3e4fd30141bd01e49
Nate Alberg <alberg.nate@gmail.com>
2016-01-08 16:47:05 -0600

    fixed app-tenant

--- dist/setup/app-tenant.js
+++ dist/setup/app-tenant.js
@@ -15,14 +15,14 @@ var appTenant = {
 
   config:function(path, makePlural){
     if(!path) return appTenant._config; // return entire config if no path
-    var value = _.getPathValue(appTenant._config, path);
-    if(!_.hasValue(value)) {
+    var value = _.get(appTenant._config, path);
+    if(value === void 0) {
       console.log('appTenant.config(' + path + ') MISSING!');
       return '';
     }
     if(makePlural) {
-      var customPluralValue = _.getPathValue(appTenant._config, path + '_plural');
-      if(_.hasValue(customPluralValue)) return customPluralValue;
+      var customPluralValue = _.get(appTenant._config, path + '_plural');
+      if(value !== void 0) return customPluralValue;
       return appTenant.makePlural(value);
     }
     return value;
