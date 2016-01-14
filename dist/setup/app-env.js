047d2d7cd6ea111100b2ae976997914a2f6c7425
Nate Alberg <alberg.nate@gmail.com>
2016-01-14 16:35:06 -0600

    appEnv

--- dist/setup/app-env.js
+++ dist/setup/app-env.js
@@ -7,8 +7,10 @@ var appEnv = {
     if(!config.ENV)           throw new Error('appEnv.init failed. ENV not defined');
     if(!config.TENANT_HASH)   throw new Error('appEnv.init failed. TENANT_HASH not defined');
     if(!config.TENANT_INDEX)  throw new Error('appEnv.init failed. TENANT_INDEX not defined');
-    appEnv._config.ENV = config;
-    console.log('appEnv', appEnv);
+    appEnv._config.ENV = config.ENV;
+    appEnv._config.TENANT_HASH = config.TENANT_HASH;
+    appEnv._config.TENANT_INDEX = config.TENANT_INDEX;
+    console.log('appEnv', appEnv._config);
   },
   ENV:function(){ return appEnv._config.ENV },
   TENANT_HASH:function(){ return appEnv._config.TENANT_HASH },
