75242f29851de6b3c965bded62dbb420ec1e7c86
Nate Alberg <alberg.nate@gmail.com>
2016-01-15 14:28:54 -0600

    appCatch appTrack updates.

--- dist/af-angular-setup.js
+++ dist/af-angular-setup.js
@@ -164,31 +164,6 @@ appTenant.get = appTenant.config; // alias
 //
 // THIS IS GLOBALLY scoped on window because we need it before angular even loads..
 //
-
-
-//
-// MIXPANEL LIB
-//
-(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
-    for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
-
-// gets browser version
-navigator.sayswho= (function(){
-  var ua= navigator.userAgent, tem,
-      M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-  if(/trident/i.test(M[1])){
-    tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-    return 'IE '+(tem[1] || '');
-  }
-  if(M[1]=== 'Chrome'){
-    tem= ua.match(/\bOPR\/(\d+)/);
-    if(tem!= null) return 'Opera '+tem[1];
-  }
-  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-  return M.join(' ');
-})();
-
 var appTrack = {
 
   loaded: false,
@@ -315,3 +290,28 @@ var appTrack = {
     appTrack.send('Page View');
   }
 };
+
+
+
+//
+// MIXPANEL LIB
+//
+(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+  for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+
+// gets browser version
+navigator.sayswho= (function(){
+  var ua= navigator.userAgent, tem,
+      M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+    tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'IE '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+    tem= ua.match(/\bOPR\/(\d+)/);
+    if(tem!= null) return 'Opera '+tem[1];
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+})();
