4df17f649f9c4bd03e9096becb42beda6179b99e
Nate Alberg <alberg.nate@gmail.com>
2016-01-21 13:41:54 -0600

    added compiled libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,102 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['concat', 'less', 'watch']);
 
+
+  var angularVersion = '1.4.9';
+
+  var concatLibs = function(filename, files){
+    return {
+      options: {
+        separator: grunt.util.linefeed + ';' + grunt.util.linefeed
+      },
+      nonull: true,
+      dest:'<%= dirs.js.out %>/'+filename,
+      src: files
+    }
+  };
+
   grunt.initConfig({
 
+    dirs:{
+      version:angularVersion,
+      js: {
+        out: 'dist/scripts/<%= dirs.version %>'
+      },
+      css:{
+        in:'src/styles',
+        out:'dist/styles'
+      }
+    },
 
     // compile less
     less:{
       'af-lib':{
         //options: {compress: true},
         files: {
-          'dist/styles/af-lib.css': 'src/styles/af-lib.less',
-          'dist/styles/af-init.css':'src/styles/af-init.less'
+          '<%= dirs.css.out %>/af-lib.css': '<%= dirs.css.in %>/af-lib.less',
+          '<%= dirs.css.out %>/af-init.css':'<%= dirs.css.in %>/af-init.less'
         }
       }
     },
 
     // compile js
     concat: {
+
       'af-lib': {
         options: {
           separator: grunt.util.linefeed + ';' + grunt.util.linefeed
-          //sourceMap:true
         },
+        nonull: true,
         files: {
-          'dist/scripts/af-angular-lib-core.js':[
+          '<%= dirs.js.out %>/af-angular-lib-core.js':[
             'src/scripts/**/*'
           ],
-          'dist/scripts/af-angular-lib-setup.js':[
+          '<%= dirs.js.out %>/af-angular-lib-setup.js':[
             'src/setup/console-fix.js',
             'src/setup/**/*'
           ],
-          'dist/scripts/af-angular-lib.js':[
+          '<%= dirs.js.out %>/af-angular-lib.js':[
             'src/setup/console-fix.js',
             'src/setup/**/*',
             'src/scripts/**/*'
           ]
         }
-      }
+      },
+
+      libs: concatLibs('af-core-libs.js', [
+        // angular
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/amplify/lib/amplify.core.js',
+        'bower_components/amplify/lib/amplify.store.js',
+        // angular
+        'bower_components/angular/angular.js',
+        'bower_components/angular-sanitize/angular-sanitize.js',
+        'bower_components/angular-animate/angular-animate.js',
+        'bower_components/angular-messages/angular-messages.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.js',
+        // util
+        'bower_components/lodash/dist/lodash.js',
+        'bower_components/moment/moment.js',
+        // raven
+        'bower_components/raven-js/dist/raven.min.js'
+      ]),
+      libsMin: concatLibs('af-core-libs.min.js', [
+        // angular
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/amplify/lib/amplify.core.min.js',
+        'bower_components/amplify/lib/amplify.store.min.js',
+        // angular
+        'bower_components/angular/angular.min.js',
+        'bower_components/angular-sanitize/angular-sanitize.min.js',
+        'bower_components/angular-animate/angular-animate.min.js',
+        'bower_components/angular-messages/angular-messages.min.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+        // util
+        'bower_components/lodash/dist/lodash.min.js',
+        'bower_components/moment/min/moment.min.js',
+        // raven
+        'bower_components/raven-js/dist/raven.min.js'
+      ])
     },
 
     // watch files... (for dev)
@@ -54,6 +114,7 @@ module.exports = function(grunt) {
     }
 
   });
+
 
 
 
