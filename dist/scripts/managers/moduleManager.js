62a78894337c5d485908f8726c775c688a84e88e
Nate Alberg <alberg.nate@gmail.com>
2016-01-21 10:13:35 -0600

    put redirectToModule in moduleManager.

--- dist/scripts/managers/moduleManager.js
+++ dist/scripts/managers/moduleManager.js
@@ -3,9 +3,10 @@
 //
 angular.module('af.moduleManager', ['_', 'af.appTenant', 'af.authManager'])
 
-    .service('afModuleManager', function(_, appTenant, afAuthManager) {
+    .service('afModuleManager', function($q, $window, _, appTenant, afAuthManager) {
 
       var system_modules = [
+        // TODO: need to differentiate portal and roadmap
         {
           key:'portal',
           enabled:true,
@@ -37,16 +38,61 @@ angular.module('af.moduleManager', ['_', 'af.appTenant', 'af.authManager'])
 
       var afModuleManager;
       return afModuleManager = {
+
+        // get list of enabled modules
         getEnabledModules:function(){
           return _.filter(system_modules, function(module){
             return module.enabled && isAuthorized(module);
           })
         },
+
+        // checks if module is enabled.
         isEnabled:function(module){
           var enabledModules = afModuleManager.getEnabledModules();
           var enabledKeys = _.map(enabledModules, 'key');
           return _.includes(enabledKeys, module);
+        },
+
+        // attempts to redirect loggedInUser to a specific module
+        redirectToModule:function(desiredModule){
+
+          var defer = $q.defer();
+
+          // whats available to user
+          var availableModules = afModuleManager.getEnabledModules();
+          if(availableModules.length == 0)
+            return defer.reject('You do not have access to any enabled modules.');
+
+          // what do they want?
+          if(!desiredModule) desiredModule = availableModules[0].key;
+          desiredModule = (''+desiredModule).toLowerCase();
+
+          if(afModuleManager.isEnabled(desiredModule)){
+            //
+            // VALID REDIRECT
+            //
+            var url = '/' + desiredModule + '/';
+            switch(desiredModule){
+              case 'portal':
+                url = '/portal/login-redirection.php';
+                break;
+              case 'metrics':
+                url = '/metrics/#/login?from=auth&sessionToken='+afAuthManager.sessionToken();
+                break;
+            }
+            $window.location = url;
+            defer.resolve(true);
+          } else {
+            //
+            // INVALID REDIRECT (return available modules)
+            //
+            defer.reject(availableModules);
+          }
+          return defer.promise;
+
         }
+
+
       }
 
     });
