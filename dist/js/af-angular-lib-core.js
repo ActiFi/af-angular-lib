7a2804615a0fa8b834dceda51c8111415fd5ccf7
Nate Alberg <alberg.nate@gmail.com>
2016-05-12 13:28:28 -0500

    bs-icon changes..

--- dist/js/af-angular-lib-core.js
+++ dist/js/af-angular-lib-core.js
@@ -212,9 +212,6 @@ angular.module('af.bsIcons', [])
 
   .directive('bsIcon', function() {
     return {
-      //compile:function(elm, attrs){
-      //  angular.element(elm).addClass('ng-show-inline glyphicon glyphicon-' + attrs.bsIcon);
-      //}
       link:function(scope, elm, attrs){
         if(attrs.bsIcon)
           angular.element(elm).addClass('ng-show-inline glyphicon glyphicon-' + attrs.bsIcon);
@@ -224,19 +221,32 @@ angular.module('af.bsIcons', [])
 
   .directive("faIcon", function() {
     return {
-      link: function(scope, elm, attrs) {
-        switch((''+attrs.faIcon).toLowerCase()){
-          case 'roadmap': attrs.faIcon = 'road'; break;
-          case 'assessment': attrs.faIcon = 'check-circle-o'; break;
-          case 'quickcontent':
-          case 'quick content':
-            attrs.faIcon = 'file-text-o'; break;
-          case 'export':  attrs.faIcon = 'file'; break;
-          case 'pdf':     attrs.faIcon = 'file-pdf-o'; break;
-          case 'rtf':     attrs.faIcon = 'file-word-o'; break;
-          case 'csv':     attrs.faIcon = 'file-excel-o'; break;
+      scope:{
+        faIcon:'='
+      },
+      compile:function(elm, attrs){
+        angular.element(elm).addClass('ng-show-inline fa');
+        var linkFunction = function(scope, elm, attrs) {
+          var getIcon = function(icon){
+            switch((''+icon).toLowerCase()){
+              case 'roadmap':     return 'road';
+              case 'assessment':  return 'check-circle-o';
+              case 'quickcontent':
+              case 'quick content':
+                return 'file-text-o';
+              case 'export':  return 'file';
+              case 'pdf':     return 'file-pdf-o';
+              case 'rtf':     return 'file-word-o';
+              case 'csv':     return 'file-excel-o';
+            }
+          };
+          scope.$watch('faIcon', function(newValue, oldValue){
+            var icon = getIcon(newValue);
+            var oldIcon = getIcon(oldValue);
+            angular.element(elm).removeClass('fa-'+oldIcon).addClass('fa-'+icon);
+          })
         }
-        angular.element(elm).addClass('ng-show-inline fa fa-' + attrs.faIcon);
+        return linkFunction;
       }
     };
   });
