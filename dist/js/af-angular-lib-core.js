0ec0b4ccd04ebba2469f25a60cb19d09a8d6d5a3
Nate Alberg <alberg.nate@gmail.com>
2016-05-12 13:39:45 -0500

    bs-icon changes..

--- dist/js/af-angular-lib-core.js
+++ dist/js/af-angular-lib-core.js
@@ -212,49 +212,27 @@ angular.module('af.bsIcons', [])
 
   .directive('bsIcon', function() {
     return {
-      scope:{
-        bsIcon:'='
-      },
       compile:function(elm, attrs){
-        angular.element(elm).addClass('ng-show-inline glyphicon');
-        var linkFunction = function(scope, elm, attrs){
-          scope.$watch('bsIcon', function(newValue, oldValue){
-            angular.element(elm).removeClass('fa-'+oldValue).addClass('fa-'+newValue);
-          })
-        };
-        return linkFunction;
+        angular.element(elm).addClass('ng-show-inline glyphicon glyphicon-' + attrs.bsIcon);
       }
     };
   })
 
   .directive("faIcon", function() {
     return {
-      scope:{
-        faIcon:'='
-      },
-      compile:function(elm, attrs){
-        angular.element(elm).addClass('ng-show-inline fa');
-        var linkFunction = function(scope, elm, attrs) {
-          var getIcon = function(icon){
-            switch((''+icon).toLowerCase()){
-              case 'roadmap':       return 'road';
-              case 'assessment':    return 'check-circle-o';
-              case 'quickcontent':
-              case 'quick content': return 'file-text-o';
-              case 'export':        return 'file';
-              case 'pdf':           return 'file-pdf-o';
-              case 'rtf':           return 'file-word-o';
-              case 'csv':           return 'file-excel-o';
-            }
-            return icon;
-          };
-          scope.$watch('faIcon', function(newValue, oldValue){
-            var icon = getIcon(newValue);
-            var oldIcon = getIcon(oldValue);
-            angular.element(elm).removeClass('fa-'+oldIcon).addClass('fa-'+icon);
-          })
-        };
-        return linkFunction;
+      compile: function(elm, attrs) {
+        switch((''+attrs.faIcon).toLowerCase()){
+          case 'roadmap': attrs.faIcon = 'road'; break;
+          case 'assessment': attrs.faIcon = 'check-circle-o'; break;
+          case 'quickcontent':
+          case 'quick content':
+            attrs.faIcon = 'file-text-o'; break;
+          case 'export':  attrs.faIcon = 'file'; break;
+          case 'pdf':     attrs.faIcon = 'file-pdf-o'; break;
+          case 'rtf':     attrs.faIcon = 'file-word-o'; break;
+          case 'csv':     attrs.faIcon = 'file-excel-o'; break;
+        }
+        angular.element(elm).addClass('ng-show-inline fa fa-' + attrs.faIcon);
       }
     };
   });
